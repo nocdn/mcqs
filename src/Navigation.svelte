1f6837d28bb4e19b75cbd807ccce91791274e3f8
Bartek Bak <bartek.bak@protonmail.com>
2025-01-23 17:16:56 +0000

    feat: now taking quiz questions from lambda function

--- src/Navigation.svelte
+++ src/Navigation.svelte
@@ -8,11 +8,13 @@
   import { ArrowLeft } from "lucide-svelte";
 </script>
 
-<div class="flex justify-between w-full max-w-xl bg-white p-5 rounded-lg">
+<div
+  class="flex justify-between item-center w-full max-w-xl bg-white p-5 rounded-lg"
+>
   <button
     onclick={onPrev}
-    class="px-5 py-2.5 mb-2 me-2 font-mono text-sm font-semibold text-gray-900 bg-white border border-gray-200 rounded-lg flex items-center gap-4
-             hover:bg-gray-100 hover:text-blue-700 focus:outline-none disabled:cursor-not-allowed disabled:opacity-70 disabled:bg-gray-200"
+    class="px-5 py-2.5 font-mono text-sm font-semibold text-gray-900 bg-white border border-gray-200 rounded-lg flex items-center gap-4
+             hover:bg-gray-100 hover:text-blue-700 focus:outline-none disabled:cursor-not-allowed disabled:opacity-70 disabled:bg-gray-200 cursor-pointer"
     disabled={disablePrev}
   >
     <ArrowLeft size={16} />
@@ -21,8 +23,8 @@
 
   <button
     onclick={onNext}
-    class="px-5 py-2.5 mb-2 me-2 font-mono text-sm font-semibold text-gray-900 bg-white border border-gray-200 rounded-lg flex items-center gap-4
-             hover:bg-gray-100 hover:text-blue-700 focus:outline-none disabled:cursor-not-allowed disabled:opacity-70 disabled:bg-gray-200"
+    class="px-5 py-2.5 font-mono text-sm font-semibold text-gray-900 bg-white border border-gray-200 rounded-lg flex items-center gap-4
+             hover:bg-gray-100 hover:text-blue-700 focus:outline-none disabled:cursor-not-allowed disabled:opacity-70 disabled:bg-gray-200 cursor-pointer"
     disabled={disableNext}
   >
     Next
